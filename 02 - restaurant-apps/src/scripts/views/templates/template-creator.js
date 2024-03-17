130a4d4c24066cda7086fc09936abccdb9ff79e4
Aria Fatah <ariafatah0711@gmail.com>
2024-03-17 21:16:39 +0700

    update

--- 02 - restaurant-apps/src/scripts/views/templates/template-creator.js
+++ 02 - restaurant-apps/src/scripts/views/templates/template-creator.js
@@ -40,6 +40,7 @@ const createRestaurantDetailTemplate = (restaurant) => `
         </div>
     </div>
 
+    <h1 class="title">daftar menu</h1>
     <div class="detail-menu">
         <div class="detail-foods">
             <h1>makanan</h1>
@@ -53,9 +54,46 @@ const createRestaurantDetailTemplate = (restaurant) => `
                 ${restaurant.menus.drinks.map((drink) => `<li>${drink.name}</li>`).join("")}
             </ul>
         </div>
- 
+    </div>
 
+    <h1 class="title">ulasan review</h1>
+    <div class="detail-review">
+        <ul>
+            ${restaurant.customerReviews
+              .map(
+                (customerReview) => `
+                    <li>
+                        <div class="review-title">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="icons">
+                                <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd" />
+                            </svg>
+                            <h1>
+                                ${customerReview.name}
+                                <span>${customerReview.date}</span>
+                            </h1>
+                        </div>
+                        <div class="review-content">
+                            <p>${customerReview.review}</p>
+                        </div>
+                    </li>
+            `
+              )
+              .join("")}
+        </ul>
+    </div>
+
+    <div class="detail-form">
+        <form class="form-review">
+            <h1>tambahkan ulasanmu</h1>
+            <label for="input-name">name: </label>
+            <input type="text" id="input-name" placeholder="nama" require autocomplete="off">
+            <label for="input-review">review: </label>
+            <textarea id="review" rows="4" placeholder="ulasan"></textarea>
+            <button type="submit">kirim</button>
+        </form>
     </div>
 `;
+
+// <input type="text" id="input-review" placeholder="review" require autocomplete="off">
 
 export { createRestaurantItemTemplate, createRestaurantDetailTemplate };
