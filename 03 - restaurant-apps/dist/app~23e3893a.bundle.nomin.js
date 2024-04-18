349c7d67b8a3753662a75b20336d5b986189d8dd
Aria Fatah <ariafatah0711@gmail.com>
2024-04-18 20:54:40 +0700

    update

--- 03 - restaurant-apps/dist/app~23e3893a.bundle.nomin.js
+++ 03 - restaurant-apps/dist/app~23e3893a.bundle.nomin.js
@@ -14,7 +14,7 @@
 /******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			id: moduleId,
+/******/ 			// no module.id needed
 /******/ 			// no module.loaded needed
 /******/ 			exports: {}
 /******/ 		};
@@ -144,17 +144,12 @@
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
 /******/ 	
-/******/ 	/* webpack/runtime/nonce */
-/******/ 	(() => {
-/******/ 		__webpack_require__.nc = undefined;
-/******/ 	})();
-/******/ 	
 /************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, [311,608,324,74,266], () => (__webpack_require__(358)))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, [949,324,74,720], () => (__webpack_require__(358)))
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
